58d868ff14d6517c8a482f562a0036223c5eb273
Jesus <jesus.cheng@gmail.com>
2014-11-20 16:10:05 -0500

    Wrote a more efficient method to compress methods

--- cracking-the-coding-interview/arrays-and-strings/problem1.5/StringCompression.java
+++ cracking-the-coding-interview/arrays-and-strings/problem1.5/StringCompression.java
@@ -5,6 +5,8 @@
 // Page 176
 
 class StringCompression {
+
+	// This is not very efficient because string concatenation takes O(n^2)
 	public static String compression(String str) {
 		int c = 1;
 		String s = "";
@@ -31,32 +33,71 @@ class StringCompression {
 		}
 	}
 
+ // More efficient
+	public static String compression2(String str) {
+		char charArray[] = new char[str.length()+1];
+		char charCount[];
+
+		int k = 0;
+		int c = 1;
+
+		for(int i=0; i<str.length(); i++) {
+			if(i == str.length()-1) {
+				charArray[k++] = str.charAt(i);
+				charCount = String.valueOf(c).toCharArray();
+
+				for(char x : charCount) {
+					if(k >= str.length()) {
+						return str;
+					}
+					charArray[k++] = x;
+				}
+				break;
+			}
+			else if(str.charAt(i) == str.charAt(i+1)) {
+				c++;
+			}
+			else {
+				charArray[k++] = str.charAt(i);
+				charCount = String.valueOf(c).toCharArray();
+				c = 1;
+
+				for(char x : charCount) {
+					if(k >= str.length()) {
+						return str;
+					}
+					charArray[k++] = x;
+				}
+			}
+		}
+		return String.valueOf(charArray);
+	}
 
 	// Main
 	public static void main(String args[]) {
 		String str = "aabcccccaaa";
 		System.out.println(str);
-		System.out.println(compression(str));
+		System.out.println(compression2(str));
 		System.out.println();
 
 		str = "abc";
 		System.out.println(str);
-		System.out.println(compression(str));
+		System.out.println(compression2(str));
 		System.out.println();
 
 		str = "abccccd";
 		System.out.println(str);
-		System.out.println(compression(str));
+		System.out.println(compression2(str));
 		System.out.println();
 
 		str = "abcc";
 		System.out.println(str);
-		System.out.println(compression(str));
+		System.out.println(compression2(str));
 		System.out.println();
 
 		str = "aaaaaaaaaaaaaaaaaaaaabcc";
 		System.out.println(str);
-		System.out.println(compression(str));
+		System.out.println(compression2(str));
 		System.out.println();
 	}
 }

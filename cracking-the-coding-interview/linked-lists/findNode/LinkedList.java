301921f7f3ba3a4cd6c3acf233e569b067cba124
jesuscheng <jesus.cheng@gmail.com>
2014-12-29 19:15:31 -0500

    Trying to implement linked list

---
+++ cracking-the-coding-interview/linked-lists/findNode/LinkedList.java
@@ -0,0 +1,59 @@
+import java.util.Iterator;
+
+public class LinkedList<Item> implements Iterable<Item> {
+	private Node first;
+
+	private class Node {
+		Item item;
+		Node next;
+
+		Node(Item item, Node next) {
+			this.item = item;
+			this.next = next;
+		}
+
+		Node(Item item) {
+			this.item = item;
+		}
+	}
+
+	public void add(Item item) {
+		while(first.next!=null) {
+			first = first.next;
+		}
+
+	}
+
+	public Iterator<Item> iterator() {
+		return new ListIterator();
+	}
+
+	private class ListIterator implements Iterator<Item> {
+		private Node current = first;
+
+		public boolean hasNext() {
+			return current != null;
+		}
+
+		public Item next() {
+			Item item = current.item;
+			current = current.next;
+			return item;
+		}
+
+		public void remove() {}
+	}
+
+
+	public static void main(String args[]) {
+		LinkedList<Integer> ll = new LinkedList<Integer>();
+
+		for(int i=1; i<=10; i++) {
+			ll.add(i);
+		}
+
+		for(Integer i : ll) {
+			System.out.print(i + " ");
+		}
+	}
+}

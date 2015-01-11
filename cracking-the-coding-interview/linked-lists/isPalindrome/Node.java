dae022bac3773b33aa009279e1ed54d002459872
jesuscheng <jesus.cheng@gmail.com>
2015-01-11 12:53:27 -0500

    Solved is palindrome problem for linked list

--- cracking-the-coding-interview/linked-lists/isPalindrome/Node.java
+++ cracking-the-coding-interview/linked-lists/isPalindrome/Node.java
@@ -1,3 +1,8 @@
+// Problem 2.7: Implement a function to check if a linked list is a palindrome
+// Page 78. Solution 196
+
+import java.util.Stack;
+
 public class Node<Item> {
 	Item item;
 	Node next;
@@ -22,8 +27,46 @@ public class Node<Item> {
 	}
 
 
+	// Problem 2.7: Implement a function to check if a linked list is a palindrome
+	public boolean isPalindrome(Node head) {
+		Node slowRunner = head;
+		Node fastRunner = head;
+
+		Stack<Item> reverse = new Stack<Item>();
+
+		// Add elements to the stack until the middle
+		while(fastRunner != null && fastRunner.next != null) {
+			reverse.push((Item) slowRunner.item);
+			slowRunner = slowRunner.next;
+			fastRunner = fastRunner.next.next;
+		}
+
+		// If the number of elements is odd we need to skip the middle element
+		if(fastRunner != null) {
+			slowRunner = slowRunner.next;
+		}
+
+		// Compare
+		while(slowRunner != null) {
+			if(reverse.peek() != slowRunner.item) {
+				return false;
+			}
+			reverse.pop();
+			slowRunner = slowRunner.next;
+		}
+		return true;
+	}
+
 	// Main
 	public static void main(String args[]) {
-		
+		Node n = new Node(0);
+		n.add(1);
+		n.add(2);
+		n.add(3);
+		n.add(2);
+		n.add(1);
+		n.add(0);
+
+		System.out.println("Is palindrome?: " + n.isPalindrome(n));
 	}
 }

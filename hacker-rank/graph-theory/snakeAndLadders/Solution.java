8132c8af61e49cf19eba55494946ee239a842cde
jesuscheng <jesus.cheng@gmail.com>
2015-01-25 16:45:06 -0500

    Added more comments to Snake and Ladders problem

--- hacker-rank/graph-theory/snakeAndLadders/Solution.java
+++ hacker-rank/graph-theory/snakeAndLadders/Solution.java
@@ -8,66 +8,71 @@ import java.math.*;
 import java.util.regex.*;
 
 public class Solution {
-    public static boolean marked[];
-    public static int edgeTo[];
-    public static LinkedList<Integer> adj[];
-    public static int count;
+	public static boolean marked[];
+	public static int edgeTo[];
+	public static LinkedList<Integer> adj[];
+	public static int count;
 
-    // Build the graph
-    public static void createGraph() {
-        adj = (LinkedList<Integer>[]) new LinkedList[101];
-        for(int i=0; i<=100; i++) {
-            adj[i] = new LinkedList<Integer>();
-        }
-		
-		// Make node point to the next six nodes because you can control de die
-        for(int i=1; i<=100; i++) {
-            if(i<=94) {
-                for(int j=1; j<=6; j++) {
-                    addEdge(i, i+j);
-                }
-            }
-            else {
-                for(int k=i+1; k<=100; k++) {
-                    addEdge(i, k);
-                }
-            }
-        }
-		
-        marked = new boolean[101];
-        edgeTo = new int[101];
-    }
-	
-    public static void bfs() {
-        LinkedList<Integer> queue = new LinkedList<Integer>();
-        queue.add(1);
-        marked[1] = true;
-		
-        while(!queue.isEmpty()) {
-            int v = queue.remove();
-			
-            for(int w : adj[v]) {
-                if(!marked[w]) {
-                    edgeTo[w] = v;
-                    marked[w] = true;
-                    queue.add(w);
-                }
-            }
-        }
-    }
-	
-    public static int countSteps() {
-        count = 0;
-        for(int x=100; x!=1; x=edgeTo[x]) {
-            count++;
+	// Build the graph
+	public static void createGraph() {
+		adj = (LinkedList<Integer>[]) new LinkedList[101];
+		for(int i=0; i<=100; i++) {
+			adj[i] = new LinkedList<Integer>();
 		}
-        return count-1;
-    }
+
+		// Make node point to the next six nodes because you can control de die
+		for(int i=1; i<=100; i++) {
+			if(i<=94) {
+				for(int j=1; j<=6; j++) {
+					addEdge(i, i+j);
+				}
+			}
+			else {
+				for(int k=i+1; k<=100; k++) {
+					addEdge(i, k);
+				}
+			}
+		}
+		
+		marked = new boolean[101];
+		edgeTo = new int[101];
+	}
 	
-    public static void addEdge(int v, int w) {
-        adj[v].add(w);
-    }
+	// Breadth First Search
+	public static void bfs() {
+		LinkedList<Integer> queue = new LinkedList<Integer>();
+		queue.add(1);
+		marked[1] = true;
+		
+		while(!queue.isEmpty()) {
+			int v = queue.remove();
+			
+			for(int w : adj[v]) {
+				if(!marked[w]) {
+					edgeTo[w] = v;
+					marked[w] = true;
+					queue.add(w);
+				}
+			}
+		}
+	}
 	
+	// Count how many steps are needed to reach a 100
+	public static int countSteps() {
+		count = 0;
+		for(int x=100; x!=1; x=edgeTo[x]) {
+			count++;
+		}
+		return count-1;
+	}
+	
+	// Add edge to the graph
+	public static void addEdge(int v, int w) {
+		adj[v].add(w);
+	}
+	
+
+	// Main
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int t = in.nextInt();

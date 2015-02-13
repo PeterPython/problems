ccd013d365716898aaaaa42d9a2de06b4cfd6b49
jesuscheng <jesus.cheng@gmail.com>
2015-02-12 22:52:13 -0500

    Working on problem 15.12 to find subset from dictionary

--- elements-of-programming-interviews/meta-algorithms/isAWord/CheckForWord.java
+++ elements-of-programming-interviews/meta-algorithms/isAWord/CheckForWord.java
@@ -12,27 +12,23 @@ public class CheckForWord {
 		for(int i=0; i<str.length(); i++) {
 			if(dictionary.contains(str.substring(0, i+1))) {
 				t[i] = i+1;
-				System.out.println(i+1);
-			} 
-			// i=6, j=2
-			//i=9, j=6
+			}
 
 			for(int j=0; j<i && t[i]==0; j++) {
 				if(t[j] != 0 && dictionary.contains(str.substring(j+1, i))) {
 					t[j] = i-j;
-					System.out.println(i-j);
 				}
 			}
 		}
 
 		LinkedList<String> ret = new LinkedList<String>();
-		// if(t.length > 0) {
-		// 	int idx = str.length() -1;
-		// 	while(idx >= 0) {
-		// 		ret.add(str.substring(idx - t[idx] + 1, t[idx]));
-		// 		idx -= t[idx];
-		// 	}
-		// }
+		if(t.length > 0) {
+			int idx = str.length() -1;
+			while(idx >= 0) {
+				ret.add(str.substring(idx - t[idx] + 1, t[idx]));
+				idx -= t[idx];
+			}
+		}
 
 		return ret;
 	}
@@ -48,6 +44,8 @@ public class CheckForWord {
 		dictionary.add("hand");
 
 		LinkedList<String> list;
-		list = checkForWord("bedbathandbeyond", dictionary);
+		// list = checkForWord("bedbathandbeyond", dictionary);
+		list = checkForWord("bedbath", dictionary);
+
 	}
 }

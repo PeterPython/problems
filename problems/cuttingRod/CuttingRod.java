f015f8d5763bea3f505d675d2e74b9fb853c9867
jesuscheng <jesus.cheng@gmail.com>
2015-02-28 20:31:56 -0500

    Solved cutting the rod problem using Dynamic Programming

---
+++ problems/cuttingRod/CuttingRod.java
@@ -0,0 +1,44 @@
+// Dynamic Programming Problem
+// Cutting a rod. Given a rod of length k and for each length 
+// there is a price (index and value of array). 
+// Calculate the maximum profit. O(n^2) with DP
+// https://www.youtube.com/watch?v=U-09Gs6cbsQ
+
+public class CuttingRod {
+	public static int cuttingRod(int a[], int length) {
+		int memo[] = new int[a.length];
+		return cuttingRod(a, length, memo);
+	}
+
+	public static int cuttingRod(int a[], int length, int memo[]) {
+		int max = 0;
+		if(length == 0) {
+			return 0;
+		}
+		if(length == 1) {
+			return a[1];
+		}
+		if(memo[length] != 0) {
+			return memo[length];
+		}
+		for(int i=1; i<=length; i++) {
+			int result = a[i] + cuttingRod(a, length-i, memo);
+			if(result > max) {
+				max = result;
+			}
+		}
+
+		memo[length] = max;
+		return max;
+	}
+
+
+	// Main
+	public static void main(String args[]) {
+		int a[] = {0,1,5,8,9,10,17,17,20};
+		System.out.println(cuttingRod(a, 2));
+		System.out.println(cuttingRod(a, 3));
+		System.out.println(cuttingRod(a, 4));
+		System.out.println(cuttingRod(a, 8));
+	}
+}

7942784a09a35605e6580206933852de05e14fac
jesuscheng <jesus.cheng@gmail.com>
2015-04-15 15:38:55 -0400

    Program to sort a hash map

---
+++ problems/sortRank/SortRank.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class SortRank {
+	public static void main(String args[]) throws IOException {
+		HashMap<Integer, String> map = new HashMap<Integer, String>();
+		File file = new File("input.txt");
+		Scanner in = new Scanner(file);
+
+		int n = Integer.parseInt(in.nextLine());
+		int a[] = new int[n];
+		for(int i=0; i<n; i++) {
+			String line[] = in.nextLine().split(" ");
+			String country = line[0];
+			int rank = Integer.parseInt(line[1]);
+
+			map.put(rank, country);
+			a[i] = rank;
+		}
+
+		Arrays.sort(a);
+		for(int i : a) {
+			System.out.println(map.get(i));
+		}
+	}
+}

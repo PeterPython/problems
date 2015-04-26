afa46847724f73ee3a15bf169b0dc8a200f65210
jesuscheng <jesus.cheng@gmail.com>
2015-04-26 19:38:01 -0400

    Wrote program to substract strings

--- problems/substractStrings/SubstractStrings.java
+++ problems/substractStrings/SubstractStrings.java
@@ -2,25 +2,56 @@
 
 public class SubstractStrings {
 	public static String substractStrings(String x, String y) {
-		// 125, 19
 		String result = "";
+		int lend = 0;
 
-		for(int i=0; i<y.length(); i++) {
-			int a = x.charAt(x.length()-i) - '0';
-			int b = y.charAt(y.length()-i) - '0';
+		int i = 0;
 
+		for(i=0; i<y.length(); i++) {
+			int a = x.charAt(x.length()-i-1) - '0';
+			int b = y.charAt(y.length()-i-1) - '0';
 			int dif = a-b;
-			if(dif < 0) {
-				
+
+			if(a == 0 && lend == 1) {   // Check if a was already 0 and had to borrow
+				a = 10;
+				lend = 1;
+				dif = a-b-lend;
 			}
+			else if(dif < 0) {
+				a = a + 10;
+				dif = a-b-lend;
+				lend = 1;
+			}
+			else {
+				dif = a-b-lend;
+				lend = 0;
+			}
+			result = dif + result;
+		}
+
+		// The rest of the string
+		for(int j=i; j<x.length(); j++) {
+			int a = x.charAt(x.length()-j-1) - '0';
+			if(a == 0 && lend == 1) {   // Check if a was already 0 and had to borrow
+				a = 10;
+				lend = 1;
+				a = a-lend;
+			} else if(lend == 1) {
+				a = a-lend;
+				lend = 0;
+			}
+
+			result = a + result;
 		}
 
 		return result;
 	}
 
+
 	public static void main(String args[]) {
-		String x = "1234";
-		int a = x.charAt(0) - '0';
-		System.out.println(a);
+		System.out.println(substractStrings("125", "19"));  // 106
+		System.out.println(substractStrings("15", "2"));  // 13
+		System.out.println(substractStrings("100", "15"));  // 85
+		System.out.println(substractStrings("1000", "15"));  // 985
 	}
 }

79c892729cd6bb750d5c526c21f4561a37f84b35
jesuscheng <jesus.cheng@gmail.com>
2015-05-11 18:06:37 -0400

    Changed max subarray problem so it works with negative numbers

--- problems/maxSubArray/MaxSubArray.java
+++ problems/maxSubArray/MaxSubArray.java
@@ -3,17 +3,17 @@
 
 class MaxSubArray {
 	public static int getMaxSum(int a[]) {
-		int k = 0;
-		int max = 0;
+		int max = Integer.MIN_VALUE;
+		int currentSum = 0;
 
 		for(int i=0; i<a.length; i++) {
-			if(k+a[i] <= 0) {
-				k = 0;
-			} else {
-				k+=a[i];
-				if(k>max) {
-					max = k;
-				}
+			currentSum += a[i];
+
+			if(currentSum > max) {
+				max = currentSum;
+			}
+			if(currentSum <= 0) {
+				currentSum = 0;
 			}
 		}
 		return max;
@@ -21,23 +21,23 @@ class MaxSubArray {
 
 	// Main
 	public static void main(String args[]) {
-		int a[] = {1,2,3,4};
+		int a[] = {1,2,3,4};  // 10
 		System.out.print(getMaxSum(a));
 		System.out.println();
 
-		int a1[] = {2,3,-1,-3};
+		int a1[] = {2,3,-1,-3};  // 5
 		System.out.print(getMaxSum(a1));
 		System.out.println();
 
-		int a2[] = {-1,5,100,-1000};
+		int a2[] = {-1,5,100,-1000};  // 105
 		System.out.print(getMaxSum(a2));
 		System.out.println();
 
-		int a3[] = {-1,-2,-3,-4};
+		int a3[] = {-5,-1,-2,-3,-4};  // -1
 		System.out.print(getMaxSum(a3));
 		System.out.println();
 
-		int a4[] = {1000,20000,-1,300000};
+		int a4[] = {1000,20000,-1,300000};  // 320999
 		System.out.print(getMaxSum(a4));
 		System.out.println();
 	}

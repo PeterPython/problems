55e05f095a0bb595e75482954d4d5b81833489cd
jesuscheng <jesus.cheng@gmail.com>
2015-05-15 19:19:02 -0400

    Created program to find pairs that add up to K with duplicates

--- problems/pairAddsToK/SumToK.java
+++ problems/pairAddsToK/SumToK.java
@@ -6,9 +6,9 @@ public class SumToK {
 	// Assuming not duplicates
 	public static int sumToK(int a[], int k) {
 		HashMap<Integer, Boolean> map = new HashMap<Integer, Boolean>();
-		int n = a.length;
 		int count = 0;
 
+		int n = a.length;
 		// Putting array into hashtable
 		for(int i=0; i<n; i++) {
 			map.put(a[i], true);
@@ -16,6 +16,8 @@ public class SumToK {
 
 		for(int i=0; i<n; i++) {
 			int dif = k - a[i];
+
+			// Check if contains dif, AND a[i] is not already checked, AND a[i] and dif are different
 			if(map.containsKey(dif) && map.get(a[i]) && a[i] != dif) {
 				count++;
 			}
@@ -25,8 +27,41 @@ public class SumToK {
 		return count;
 	}
 
+	// With Duplicates
+	public static int countPairs(int a[], int k) {
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		int count = 0;
+
+		int n = a.length;
+
+		for(int i=0; i<n; i++) {
+			if(!map.containsKey(a[i])) {
+				map.put(a[i], 1);
+			}
+			else {
+				map.put(a[i], map.get(a[i]) + 1);
+			}
+		}
+
+		for(int i=0; i<n; i++) {
+			int dif = k - a[i];
+
+			if(map.containsKey(dif) && map.get(a[i]) > 0) {
+				map.put(dif, map.get(dif) - 1);
+				count++;
+			}
+		}
+
+		return count;
+	}
+
+
 	public static void main(String args[]) {
 		int a[] = {1,2,3,4,5};
 		System.out.println(sumToK(a, 6));
+		System.out.println(countPairs(a, 6));
+
+		int b[] = {2,2,3,3};
+		System.out.println(countPairs(b, 5));
 	}
 }

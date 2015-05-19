c21f755f534c65d2c5c2b1f52e2b8cd9bf0cc831
jesuscheng <jesus.cheng@gmail.com>
2015-05-19 15:34:35 -0400

    Solved kata for who won the election

---
+++ codewars/who-whon-the-election/election-winner.py
@@ -0,0 +1,18 @@
+def getWinner(listOfBallots):
+  map = {}
+  winning = ""
+  max = 0;
+  for cand in listOfBallots:
+    if cand in map:
+      map[cand] = map[cand]+1
+    else:
+      map[cand] = 1
+    
+    if map[cand] > max:
+      max = map[cand]
+      winning = cand
+      
+  if max > len(listOfBallots)/2:
+    return winning
+  else:
+    return None

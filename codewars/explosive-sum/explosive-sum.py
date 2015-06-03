ea3da470d1b34413a1511d21c0e48583ff00150e
Jesus Cheng <jesus.cheng@gmail.com>
2015-06-03 16:52:24 -0400

    Added comments to explosive sum

--- codewars/explosive-sum/explosive-sum.py
+++ codewars/explosive-sum/explosive-sum.py
@@ -1,4 +1,6 @@
 # http://www.codewars.com/kata/explosive-sum
+# How many ways/combinations to sum to n using numbers from 1 to n
+# Ex. exp_sum(3) -> 1+1+1, 2+1
 
 def exp_sum(n):
     return e_sum(n, 0, 1)
@@ -22,6 +24,7 @@ def exp_sum_memo(n):
     memo = [[0 for x in range(n+1)] for x in range(n+1)]
     return e_sum_memo(n, 1, memo)
 
+# index is where the loop should start and increase it by 1 so it does not repeated
 def e_sum_memo(n, index, memo):
     if n == 0:
         return 1

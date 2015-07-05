a17c5d75c9051143bf4ecbc763fb81b03bda8de4
Jesus Cheng <jesus.cheng@gmail.com>
2015-07-04 20:23:28 -0400

    Added comments to fibonacci

--- problems/fibonacci/IterativeFibonacci.java
+++ problems/fibonacci/IterativeFibonacci.java
@@ -1,14 +1,6 @@
-class IterativeFibonacci {
-	public static void main(String args[]) {
-		System.out.println("Fibonacci: ");
-		System.out.println("Fibonacci(0) = " + fibonacci(0));
-		System.out.println("Fibonacci(1) = " + fibonacci(1));
-		System.out.println("Fibonacci(2) = " + fibonacci(2));
-		System.out.println("Fibonacci(3) = " + fibonacci(3));
-		System.out.println("Fibonacci(4) = " + fibonacci(4));
-		System.out.println("Fibonacci(5) = " + fibonacci(5));
-	}
+// Iterative Fibonacci 
 
+class IterativeFibonacci {
 	private static int fibonacci(int n) {
 		int f=0, f1=0, f2=1;
 		if(n == 1) {
@@ -21,5 +13,15 @@ class IterativeFibonacci {
 			}
 		}
 		return f;
+	}
+
+	public static void main(String args[]) {
+		System.out.println("Fibonacci: ");
+		System.out.println("Fibonacci(0) = " + fibonacci(0));
+		System.out.println("Fibonacci(1) = " + fibonacci(1));
+		System.out.println("Fibonacci(2) = " + fibonacci(2));
+		System.out.println("Fibonacci(3) = " + fibonacci(3));
+		System.out.println("Fibonacci(4) = " + fibonacci(4));
+		System.out.println("Fibonacci(5) = " + fibonacci(5));
 	}
 }
--- problems/fibonacci/RecursiveFibonacci.java
+++ problems/fibonacci/RecursiveFibonacci.java
@@ -1,14 +1,6 @@
-class RecursiveFibonacci {
-	public static void main(String args[]) {
-		System.out.println("Fibonacci: ");
-		System.out.println("Fibonacci(0) = " + fibonacci(0));
-		System.out.println("Fibonacci(1) = " + fibonacci(1));
-		System.out.println("Fibonacci(2) = " + fibonacci(2));
-		System.out.println("Fibonacci(3) = " + fibonacci(3));
-		System.out.println("Fibonacci(4) = " + fibonacci(4));
-		System.out.println("Fibonacci(5) = " + fibonacci(5));
-	}
+// Recursive Fibonacci
 
+class RecursiveFibonacci {
 	private static int fibonacci(int n) {
 		if(n==0) {
 			return 0;
@@ -17,5 +9,15 @@ class RecursiveFibonacci {
 		} else {
 			return fibonacci(n-1) + fibonacci(n-2);
 		}
+	}
+
+	public static void main(String args[]) {
+		System.out.println("Fibonacci: ");
+		System.out.println("Fibonacci(0) = " + fibonacci(0));
+		System.out.println("Fibonacci(1) = " + fibonacci(1));
+		System.out.println("Fibonacci(2) = " + fibonacci(2));
+		System.out.println("Fibonacci(3) = " + fibonacci(3));
+		System.out.println("Fibonacci(4) = " + fibonacci(4));
+		System.out.println("Fibonacci(5) = " + fibonacci(5));
 	}
 }
